1f0f51c62b75b7155cda1fedf9682db079aeb448
Mikhail Nikolaenko <nyanpepsi@gmail.com>
2014-09-16 00:29:25 +0400

    upd
    upd

--- Information Security/Task1/sigFind.py
+++ Information Security/Task1/sigFind.py
@@ -6,4 +6,21 @@ def getSign(file, offset, byteCount):
     return s
 
 def checkCopies(file, searchTarget, offset, byteCount):
+    import os
+    contents = os.listdir(searchTarget)
+    signature = getSign(file, offset, byteCount)
+    res = []
+
+    for dirname, subdirname, files in os.walk(searchTarget):
+        for f in files:
+            with open(dirname + '\\' + f, 'rb') as checkFile:
+                return checkFile.read()
+                if signature in checkFile.read():
+                    return checkFile.read()
+                    #res.append(os.path.normpath(dirname) + f)
+    return res
+
+def findSign(signature, file):
+    with open(file, 'rb') as f:
+        
     
